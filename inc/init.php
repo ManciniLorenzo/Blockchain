69681e2d17be47bdd4a4e00309878a6e7b1c8552
MancioLollo <mancinilorenzo2000.lm@gmail.com>
2022-02-17 15:01:10 +0100

    inc init

---
+++ inc/init.php
@@ -0,0 +1,7 @@
+<?php
+
+if (!defined('ROOT_URL')) define('ROOT_URL', 'http://localhost/Blockchain/');
+    
+if (!defined('ROOT_PATH')) define('ROOT_PATH', 'C:\\xampp\\htdocs\\Blockchain\\');
+
+?>
